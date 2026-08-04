0d29775a322c74d58dc936fff5de504e28816567
mikheevapa <mikheeva.p.a@gmail.com>
2026-08-04 20:21:59 +0300

    Update stack.c

--- lessons/homework/stack.c
+++ lessons/homework/stack.c
@@ -106,3 +106,92 @@ double value(list * lst)
 {
 	return lst->tail->value;
 }
+
+
+
+
+
+int data_check (operation * number, double * a, double * b)
+{
+    char op = 0;
+
+
+    scanf(" %lf %lf %c", a, b, &op);
+
+    if (op != '+' && op != '-' && op != '*' && op != '/')
+    {
+        printf("I don't know this operation :(.\n");
+        return 1;
+    }
+    
+    *number = function_op(op);
+
+    if (*number == DIV && fabs(*b) < 1e-15)
+    {
+        printf("Division by zero\n");
+        return 2;
+    }
+
+    return 0;
+}
+
+operation function_op(char op)
+{
+    switch (op)
+    {
+    case '+' :
+        return SUM;
+    case '-' :
+        return SUB;
+    case '*' :
+        return MUL;
+    case '/' :
+        return DIV;
+
+    default:
+        assert(0);
+    }
+}
+
+func function (operation number)
+{
+    switch (number)
+    {
+    case SUM:
+        return sum;
+
+    case SUB:
+        return sub;
+
+    case MUL:
+        return mul;
+
+    case DIV:
+        return div;
+
+    default:  
+        assert(0);
+    }
+
+    return NULL;
+}
+
+double sum(double a, double b)
+{
+    return a+b;
+}
+
+double sub(double a, double b)
+{
+    return a-b;
+}
+
+double mul(double a, double b)
+{
+    return a*b;
+}
+
+double div(double a, double b)
+{
+    return a/b;
+}
