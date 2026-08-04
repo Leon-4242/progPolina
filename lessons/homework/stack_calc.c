41c640f4eedc749ac03bec73b53eeac85293803a
mikheevapa <mikheeva.p.a@gmail.com>
2026-08-04 20:21:06 +0300

    Update stack_calc.c

--- lessons/homework/stack_calc.c
+++ lessons/homework/stack_calc.c
@@ -34,3 +34,41 @@ int main(void)
 // 6 3 /
 //7 8 1 * +
 //7+8*1
+
+
+#include "stack.h"
+
+int main (void)
+{
+    list * stack = make_list();
+    operation number = SUM;
+    double a = 0, b = 0, buff = 0;
+    func f;
+
+    while (1)
+    {
+        if (data_check (&number, &a, &b) != 0)
+        {
+            continue;
+        }
+
+        push_back(stack, a);
+        push_back(stack, b);
+
+        f = function(number);
+
+        buff = value(stack);
+        pop_back(stack);
+
+        buff = f(value(stack), buff);
+        pop_back(stack);
+
+        push_back(stack, buff);
+
+    }
+
+    del_list(stack);
+    return 0;
+}
+
+
