8f106d94c892bc4e0828a8df210b6312c081ce12
Leon-4242 <leon.zazovskii@math.msu.ru>
2026-08-19 12:40:27 +0300

    add func_sort

--- lessons/homework/array8.cpp
+++ lessons/homework/array8.cpp
@@ -75,3 +75,20 @@ void function(double* arr, int* n)
 
     *n = new_n;
 }
+
+void function_sort(double * arr, int * n)
+{
+	int k = 0;
+
+	for (int i = 0; i < *n - 1; ++i)
+	{
+		if (arr[i] == arr[i+1])
+		{
+			continue;
+		}
+
+		arr[++k] = arr[i+1];
+	}
+
+	*n = k+1;
+}
