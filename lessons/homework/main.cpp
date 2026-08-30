271bba913b5dab510ca1c17ffa6c01fe1d7147f7
mikheevapa <mikheeva.p.a@gmail.com>
2026-08-30 17:35:58 +0300

    Refactor output statements in main.cpp
    Updated output statements to use operator overloading for LongInt.

--- lessons/homework/main.cpp
+++ lessons/homework/main.cpp
@@ -12,29 +12,19 @@ int main()
     LongInt g(9);
     LongInt h(-19);
 
-    LongInt i = -a; 
-    LongInt j = c - d; 
-    LongInt k = e - f; 
-    LongInt l = g - h; 
 
-    std::cout << "a = " << a.view() << std::endl;
-    std::cout << "b = " << b.view() << std::endl;
-    std::cout << "c = " << c.view() << std::endl;
-    std::cout << "d = " << d.view() << std::endl;
-    std::cout << "e = " << e.view() << std::endl;
-    std::cout << "f = " << f.view() << std::endl;
-    std::cout << "g = " << g.view() << std::endl;
-    std::cout << "h = " << h.view() << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "c = " << c << std::endl;
+    std::cout << "d = " << d << std::endl;
+    std::cout << "e = " << e << std::endl;
+    std::cout << "f = " << f << std::endl;
+    std::cout << "g = " << g << std::endl;
+    std::cout << "h = " << h << std::endl;
 
-    std::cout << "-a = " << i.view() << std::endl;
-    std::cout << "c - d = " << j.view() << std::endl;
-    std::cout << "e - f = " << k.view() << std::endl;
-    std::cout << "g - h = " << l.view() << std::endl;
-
-    std::cout << a.print(b) << std::endl;
-    std::cout << e.print(f) << std::endl; 
-    std::cout << c.print(d) << std::endl; 
-
+	std::cout << "c - d = " << c - d << std::endl;
+    std::cout << "e - f = " << e - f << std::endl;
+    std::cout << "g - h = " << g - h << std::endl;
 
     return 0;
 }
